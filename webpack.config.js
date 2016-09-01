baa86d84dd2ba5a0f91d318afa04656b4dd171a1
Jan Haering <hhj@centrum.cz>
2016-09-01 08:48:04 +0200

    chore: add webpack, clean up dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,41 @@
 'use strict';
 
 var webpack = require('webpack')
+var prefixLoaders = 'css-loader!postcss-loader'
 
 var env = process.env.NODE_ENV
 var config = {
   module: {
     loaders: [
-      { test: /\.js$/, loaders: ['babel-loader'], exclude: /node_modules/ }
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel-loader'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(gif|jpg|png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
+      },
+      {
+        test: /\.css$/,
+        loader: prefixLoaders,
+      },
+      {
+        test: /\.less$/,
+        loader: `${prefixLoaders}!less-loader`,
+      },
+      {
+        test: /\.scss$/,
+        loader: `${prefixLoaders}!sass-loader`,
+      },
+      {
+        test: /\.styl$/,
+        loader: `${prefixLoaders}!stylus-loader`,
+      }
     ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
   },
   output: {
     library: '@hhjcz/react-lib',
